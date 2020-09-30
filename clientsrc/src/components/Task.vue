5abbecb9e4d9016e42d7a4ac90c36ef45974ffe9
tim hopp <timothyjhopp@gmail.com>
2020-09-30 15:11:50 -0600

    worked on currentTask modal

--- clientsrc/src/components/Task.vue
+++ clientsrc/src/components/Task.vue
@@ -7,12 +7,12 @@
       data-target="#taskModal"
       @dragstart="moveTask()"
     >
-      <div class="bg-secondary text-white rounded mt-2 p-2">
-        <div class="row justify-content-between ml-2 mt-1 mr-3">
-          <h5 class="text-left">{{ task.title }}</h5>
-          <button @click="deleteTask(task._id)" class="btn btn-outline-danger">
-            Delete
-          </button>
+      <div class="bg-secondary text-white rounded mt-2">
+        <div class="row justify-content-between ml-2 mt-1 mr-3 pt-2">
+          <h5 class="text-left pl-2">{{ task.title }}</h5>
+        </div>
+        <div class="row justify-content-center">
+          <p class="text-left">{{ task.comments.length }} Comments</p>
         </div>
       </div>
     </div>
@@ -39,15 +39,12 @@ export default {
     },
   },
   methods: {
-    deleteTask(taskId) {
-      // let task = this.$store.state.tasks.find(task => commentId == task.comments.includes(comment.commentId))
-      this.$store.dispatch("deleteTask", taskId);
-    },
     moveTask() {
       event.dataTransfer.setData("data", JSON.stringify(this.task));
       event.dataTransfer.setData("list", this.task.listId);
     },
     setCurrentTask(taskId) {
+      //Need to add boardId to currentTask
       this.$store.dispatch("currentTask", taskId);
       this.$router.push({
         name: "currentTask",
